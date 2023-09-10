8a12fc398270a1a9f70554a30c4aaf145a42344d
pdao1 <69880265+pdao1@users.noreply.github.com>
2023-09-09 14:06:16 -1000

    Added main route

--- credit-card-manager-front/src/App.jsx
+++ credit-card-manager-front/src/App.jsx
@@ -8,11 +8,14 @@ import {
 } from "react-router-dom";
 
 import PersonDashboard from './components/PersonDashboard';  // Import this component from its file
+import MainPage from './components/MainPage';
 
 function App() {
+  
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<MainPage />} />
         <Route path="/:person" element={<PersonDashboard />} />
       </Routes>
     </Router>
