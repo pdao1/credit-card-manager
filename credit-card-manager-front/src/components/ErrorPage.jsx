5fdae4b6b03b8835d4907dcc6ca461b4488d94a9
pdao1 <69880265+pdao1@users.noreply.github.com>
2023-09-09 21:25:52 -1000

    one last time before bed

--- credit-card-manager-front/src/components/ErrorPage.jsx
+++ credit-card-manager-front/src/components/ErrorPage.jsx
@@ -7,15 +7,16 @@ const ErrorPage = () => {
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600">404</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">This is most likely because you are trying to access a path/unique instance with an ID less than 8 characters (example: notyoursandbox.com/ID12345 = 7 characters). Please return to <a href="./">Home</a> to generate a unique 12 digit ID instance or manually decide on one and appending it at the end, like this: https://notyoursandbox.com/whateverYouWantItToBe6969</p>
+          <p className="mt-6 text-base leading-7 text-gray-600">This is most likely because you are trying to access a path/unique instance with an ID less than 8 characters (example: notyoursandbox.com/ID12345 = 7 characters). </p>
+          <p className="mt-6 text-base leading-7 text-gray-600">Please return to <a href="./">Home</a> to generate a unique 12 digit ID instance or manually decide on one and appending it at the end, like this: https://notyoursandbox.com/whateverYouWantItToBe6969</p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
-              href="#"
+              href="https://notyoursandbox.com"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Go back home
             </a>
-            <a href="#" className="text-sm font-semibold text-gray-900">
+            <a href="mailto:lmfao@thereisNONE.com" className="text-sm font-semibold text-gray-900">
               Contact support <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
