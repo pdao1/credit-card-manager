82147c093b98115c3900a52aa848884825cee66d
pdao1 <69880265+pdao1@users.noreply.github.com>
2023-09-09 19:46:18 -1000

    added additional instructions

--- credit-card-manager-front/src/components/MainPage.jsx
+++ credit-card-manager-front/src/components/MainPage.jsx
@@ -22,8 +22,9 @@ const MainPage = () => {
 
   return (
     <div className="container">
-      <p className="pt-5 px-2">Hit Generate Page to create a randomly-generated unique instance.  Be sure to SAVE the generated URL to return to manage your cards.</p>
-      <button className="btn btn-primary" id="generate-id" onClick={handleButtonClick}>Generate Page</button>
+      <sub className="pt-5 px-2">Hit the Generate button below to create a randomly-generated, unique instance ID. <br/>
+       <strong>IMPORTANT:</strong>Be sure to SAVE the generated URL to return to your saved instance to continue managing your cards.</sub>
+      <button className="btn btn-secondary" id="generate-id" onClick={handleButtonClick}>Generate a Unique Instance ID to Begin.</button>
     </div>
   );
 };
